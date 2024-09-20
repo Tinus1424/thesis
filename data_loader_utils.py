f948d1b783a9bc2b074e8ae190c945437652294f
tinus1424 <martijnqvtz@outlook.com>
2024-09-20 14:36:41 +0200

    restructure and modify gitignore

--- data_loader_utils.py
+++ data_loader_utils.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 """
-
-
-
-
-
-"""
-
-""" 
 # Copyright (c) 2021 Bosch Rexroth AG
 # SPDX-License-Identifier: BSD-3-Clause
 
 Author: Mohamed-Ali Tnani (Mohamed-ali.tnani@boschrexroth.de)
 Date: November 24, 2021
 
-This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, 
+including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
+In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages 
+(including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) 
+however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) 
+arising in any way out of the use of this software, even if advised of the possibility of such damage.
 """
 
 import os
