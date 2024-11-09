aec04b9ee7aced6b94683b360773433efd435d21
Tinus1424 <martijnqvtz@outlook.com>
2024-11-09 14:18:19 +0100

    Track large files with Git LFS

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import itertools
 import numpy as np
 import pandas as pd
 import data_loader_utils
+from random import shuffle
 from pathlib import Path
 import matplotlib.pyplot as plt 
 
-from cv2 import resize
-from pywt import cwt
 
-
+from numba import jit
 
 def preprocess_data(X_data, y_data):
     """
@@ -118,43 +117,51 @@ def plot_class_dist(y, features):
         plt.show()
 
 
-def augment(T, X_train, p):
+def augment(T, X_data, p):
     """
     Augments X_train
 
     Parameters:
     - T: List of augmentation functions
-    - X_train: Array of training examples
+    - X_data: Array of training examples
     - p: Number of windows
 
     Returns:
-    - X_augmented_arr: Array of augmented training examples
+    - X_augmented: Array of augmented training examples
 
     """
-    X = np.copy(X_train)
+    X = np.copy(X_data)
     X_augmented_list = []
     n_samples, n_timesteps, n_sensors = X.shape
 
+    loc = []
+    trans = []
+    
     b = compute_windows(n_timesteps, p) # Compute window bounds for p windows
 
-    for t in T:
-        print(f"Processing {t} transformations")
+    for i, t in enumerate(T):
+        print(f"Processing {t.__name__} transformations")
         for x in X:
-            W_j = np.random.randint(1, p + 1) # Randomly choose a window W_j
-
-            b_lower, b_upper = b[W_j] # Get the upper and lower bound of the windows
-            c1 = np.random.randint(b_lower, b_upper + 1) # Sample two random numbers within the window
-            c2 = np.random.randint(b_lower, b_upper + 1)
-
-            if c1 > c2: # Ensure c1 > c2
+            W_j = np.random.randint(1, p + 1)  # Randomly choose a window W_j
+            b_lower, b_upper = b[W_j]  # Get the upper and lower bound of the windows
+            c1, c2 = sorted(np.random.randint(b_lower, b_upper + 1, size=2))  # Sample two random numbers within the window
+            if c2 < c1:
                 c1, c2 = c2, c1
-                
-            x_augmented = t(x, c1, c2, p, b) # Augment x in window W_j with Ti 
+            x_augmented = t(x, c1, c2, p, b)  # Augment x in window W_j with t 
             x_scalogram = compute_scalogram(x_augmented, n_sensors)
             X_augmented_list.append(x_scalogram)
-
-    X_augmented = np.array(X_augmented_list)
-    return X_augmented_arr
+            if i == 0:
+                loc.append(0)
+            else:
+                loc.append(W_j)
+            
+        i += 1
+        
+    shuffled_indices = np.random.permutation(len(X_augmented_list))
+    
+    return (np.array(X_augmented_list)[shuffled_indices],
+            np.array(loc)[shuffled_indices],
+            np.array(trans)[shuffled_indices])
 
 
 def compute_windows(n_timesteps, p):
@@ -175,19 +182,22 @@ def compute_windows(n_timesteps, p):
         
     return windows
 
+
 def identity(x, c1, c2, p = None, b = None):
     """
     Helper function for augment()
 
     """
-    return x
+    x_aug = np.copy(x)
+    return x_aug
 
 def cut_paste(x, c1, c2, p , b):
     """
     Helper function for augment()
 
     """
-    cut_snippet = x[c1:c2, :]
+    x_aug = np.copy(x)
+    cut_snippet = x_aug[c1:c2, :]
     delta = c2 - c1
     
     W_j = np.random.randint(1, p + 1)
@@ -196,17 +206,18 @@ def cut_paste(x, c1, c2, p , b):
     p1 = np.random.randint(b_lower_j, b_upper_j - delta + 1)
     p2 = p1 + delta
 
-    x[p1:p2, :] = cut_snippet
-    return x
-    
+    x_aug[p1:p2, :] = cut_snippet
+    return x_aug
+ 
 def mean_shift(x, c1, c2, p = None, b = None):
     """
     Helper function for augment()
 
     """
-    time_series_mean = x[c1:c2].mean(axis=0)
-    x[c1:c2] = x[c1:c2] + time_series_mean
-    return x
+    x_aug = np.copy(x)
+    time_series_mean = x_aug.mean()
+    x_aug[c1:c2] = x_aug[c1:c2] + time_series_mean
+    return x_aug
 
 
 def missing_signal(x, c1, c2, p = None, b = None):
@@ -214,9 +225,13 @@ def missing_signal(x, c1, c2, p = None, b = None):
     Helper function for augment()
 
     """
-    constant = x[c1].copy()
-    x[c1:c2] = constant
-    return x
+    x_aug = np.copy(x)
+    x_aug[c1:c2] = x_aug[c1]
+    return x_aug
+
+from sklearn.preprocessing import MinMaxScaler
+from cv2 import resize
+from pywt import cwt
 
 def compute_scalogram(x, n_sensors):
     """
@@ -224,15 +239,21 @@ def compute_scalogram(x, n_sensors):
 
     """
     x_scalogram_list = []
-    
+    scaler = MinMaxScaler()
     for m in range(n_sensors):
         cwtmatr, freqs = cwt(x[:, m], np.arange(1, 129), "morl")
         cwtmatr_reshaped = resize(cwtmatr, (128, 512))
-        x_scalogram_list.append(cwtmatr_reshaped)
-        
+        cwtmatr_reshaped_normalized = scaler.fit_transform(cwtmatr_reshaped)
+        x_scalogram_list.append(cwtmatr_reshaped_normalized)
     x_scalogram_raw = np.array(x_scalogram_list)
     x_scalogram = x_scalogram_raw.reshape(128, 512, 3)
     return x_scalogram
 
 
+def shuffle(X, loc, trans):
+    p = np.random.permutation(len(X))
+    X = np.asarray(X)
+    loc = np.asarray(loc)
+    trans = np.asarray(trans)
+    return X[p], loc[p], trans[p]
 
